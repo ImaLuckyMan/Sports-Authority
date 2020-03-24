e5d5d5161d096222cda655429cf98672e224c1e6
Anthony Simon <anthony.simon2020@outlook.com>
2020-03-24 13:49:18 -0700

    Add project files.

---
+++ Controllers/ProductControllers.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsAuthority.Models;
+
+namespace SportsAuthority.Controllers
+{
+    public class ProductController : Controller
+    {
+      
+        private IProductRepository repository;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List() => View(repository.Products);
+
+    }
+}
---
+++ Models/FakeProductRepostiory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsAuthority.Models
+{
+    public class FakeProductRepostiory : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product> {
+        new Product { Name = "Simon Signature Model Football", Price = 25 },
+        new Product { Name = "Surfboard", Price = 179 },
+        new Product { Name = "Running Shoes", Price = 95 },
+        }.AsQueryable<Product>();
+    }
+}
